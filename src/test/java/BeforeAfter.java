febf51255ff2411c059f7fc57c5d743c74a46e14
LenousikK <liloo-19@yandex.ru>
2019-05-10 01:45:28 +0300

    LoginToTutByTest and LogOutTutByTest classes are created based on Page Object pattern.

---
+++ src/test/java/BeforeAfter.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BeforeAfter {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeEach
+    public void openBrowser() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 30);
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/test/java/LogOutTutByTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LogOutTutByTest extends BeforeAfter {
+    @Test
+    public void logoutSuccessful() {
+        LoginPage loginPage = new LoginPage(driver, wait);
+        loginPage.login();
+        loginPage.linkLogoutMenuItem().click();
+        loginPage.buttonLogout().click();
+        assertEquals("Войти", loginPage.getTextOfLabelLoggedOut());
+    }
+}
